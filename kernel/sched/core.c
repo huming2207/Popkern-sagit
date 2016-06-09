f621ec77be776e6c62e4daf9b15d80f3ae2eb41a
Andrey Ryabinin <aryabinin@virtuozzo.com>
2016-06-09 15:20:05 +0300

    UPSTREAM: kernel/sysrq, watchdog, sched/core: Reset watchdog on all CPUs while processing sysrq-w
    commit 57675cb976eff977aefb428e68e4e0236d48a9ff upstream.
    
    Lengthy output of sysrq-w may take a lot of time on slow serial console.
    
    Currently we reset NMI-watchdog on the current CPU to avoid spurious
    lockup messages. Sometimes this doesn't work since softlockup watchdog
    might trigger on another CPU which is waiting for an IPI to proceed.
    We reset softlockup watchdogs on all CPUs, but we do this only after
    listing all tasks, and this may be too late on a busy system.
    
    So, reset watchdogs CPUs earlier, in for_each_process_thread() loop.
    
    Signed-off-by: Andrey Ryabinin <aryabinin@virtuozzo.com>
    Cc: Linus Torvalds <torvalds@linux-foundation.org>
    Cc: Peter Zijlstra <peterz@infradead.org>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Link: http://lkml.kernel.org/r/1465474805-14641-1-git-send-email-aryabinin@virtuozzo.com
    Signed-off-by: Ingo Molnar <mingo@kernel.org>
    Signed-off-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    
    (cherry picked from commit dc20f3244ae920430d9d9f19939a13a0279380ca)
    (cherry picked from commit 8a96375c3859f03e74a2ca7c285628dfd0f2206f)
    
    Bug: 38436120
    Change-Id: I0e94012fb3ee86796d6e8b6af5c66b4d7098e782
    Signed-off-by: Francisco Franco <franciscofranco.1990@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -5374,8 +5374,6 @@ void show_state_filter(unsigned long state_filter)
 			sched_show_task(p);
 	}
 
-	touch_all_softlockup_watchdogs();
-
 #ifdef CONFIG_SCHED_DEBUG
 	sysrq_sched_debug_show();
 #endif
