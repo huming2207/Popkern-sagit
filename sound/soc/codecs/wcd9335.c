6fc516139a4db8cd74dfb519fcb5adb26bc9a73f
xiaobai <fengz181x@126.com>
2017-10-22 16:07:43 +0800

    Merge tag 'LA.UM.5.7.r1-10900-8x98.0' into cm-14.1-merge
    "LA.UM.5.7.r1-10900-8x98.0"

--- sound/soc/codecs/wcd9335.c
+++ sound/soc/codecs/wcd9335.c
@@ -5905,8 +5905,6 @@ static int tasha_codec_enable_dec(struct snd_soc_dapm_widget *w,
 					    CF_MIN_3DB_150HZ << 5);
 		/* Enable TX PGA Mute */
 		snd_soc_update_bits(codec, tx_vol_ctl_reg, 0x10, 0x10);
-		/* Disable APC */
-		snd_soc_update_bits(codec, dec_cfg_reg, 0x08, 0x00);
 		break;
 	case SND_SOC_DAPM_POST_PMU:
 		snd_soc_update_bits(codec, hpf_gate_reg, 0x01, 0x00);
